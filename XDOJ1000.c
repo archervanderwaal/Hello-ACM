2e5af874f6bc9d1f4e6fccbd6b04a980f77720b1
StormMaybin <1325338799@qq.com>
2016-05-16 19:57:58 +0800

    2016-05-16

---
+++ XDOJ1000.c
@@ -0,0 +1,15 @@
+﻿/**
+*@author:StromMaybin
+*@time:20160412
+*@filename:XDOJ1000.c 
+*/ 
+# include <stdio.h>
+
+int main (void)
+{
+	int a,b;
+	while (scanf ("%d %d",&a,&b)!=EOF)
+	{
+		printf ("%d\n",a+b);
+	}
+}
