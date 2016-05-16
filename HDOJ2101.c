058de182a188c0875142fa21c6aa6d0d96bd3daa
StormMaybin <1325338799@qq.com>
2016-05-16 20:38:00 +0800

    2016-05-16

---
+++ HDOJ2101.c
@@ -0,0 +1,23 @@
+﻿/**
+*@author:StormMaybin
+*@Date:2016-05-16
+*@Filename:HDOJ2101.c
+*@<description>签到题</description>
+*/
+# include <stdio.h>
+
+int main (void)
+{
+	int a, b, n;
+	while (scanf ("%d %d",&a, &b) == 2)
+	{
+		n = a + b;
+		if (n % 86 == 0)
+		{
+			printf ("yes\n");
+		}
+		else
+			printf ("no\n");
+	}
+	return 0;
+}
